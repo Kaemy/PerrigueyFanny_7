e3370c0489eab5a5e3dc78dd634f8d6444e139c5
Kaemy <fanny.perriguey@gmx.com>
2021-05-25 22:54:45 +0200

    correction js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-import { RECIPES } from "./data/recipesData";
-import { DataFetcher } from "./data/dataFetcher";
-import { PageManager } from "./components/pageManager";
+import { RECIPES } from "./data/recipesData.js";
+import { DataFetcher } from "./data/dataFetcher.js";
+import { PageManager } from "./pages/pageManager.js";
 
 const dataFetcher = new DataFetcher(RECIPES);
 const recipesList = dataFetcher.getRecipesList();
