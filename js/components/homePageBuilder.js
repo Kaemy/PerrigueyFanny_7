2cfc3db3e91db0578d95ad9fb419c04c8c9cb50a
Kaemy <fanny.perriguey@gmx.com>
2021-05-28 10:54:59 +0200

    correction js

--- js/components/homePageBuilder.js
+++ js/components/homePageBuilder.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { RecipeCard } from "./components/cards.js";
+import { RecipeCard } from "./cards.js";
 import { removeAccents } from "../utiles/strings.js";
 
 const BREAKPOINTS = {
